07d7e7b295953befc15750d6fb5848c7cb390e61
langyanjun <langyanjun0510@a-su.com.cn>
2026-05-23 17:15:59 +0800

    esp32s3 usb cdc ncm net is ok

---
+++ auto.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+ESP_DEV=/dev/ttyACM0
+BOARD=esp32s3_szpi/esp32s3/procpu
+PRJ=cdc
+
+build_all() {
+    if [ ! -z $1 ] ; then
+        PRJ=$1
+    fi
+    west build -p always -b ${BOARD} --sysbuild ${PRJ} -DEXTRA_CONF_FILE=overlay-usbd.conf
+}
+
+flash_esp() {
+    west flash --esp-device ${ESP_DEV}
+}
+
+monitor_esp() {
+    west espressif monitor -p ${ESP_DEV}
+}
+
+erase_and_flash_esp() {
+    west flash --erase --esp-device ${ESP_DEV}
+}
+
